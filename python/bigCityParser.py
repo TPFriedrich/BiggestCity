d27a76d38650888a130fa1cebccd02cbcacfcd54
Tomek Wieszczura <twieszczura@oc8070277704.ibm.com>
2018-07-16 15:51:40 +0200

    First step in retrieving data from webpage using BS4 library

--- python/bigCityParser.py
+++ python/bigCityParser.py
@@ -1,14 +1,15 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import lxml
 
 html = urllib.request.urlopen("http://www.citymayors.com/statistics/largest-cities-population-125.html").read()
-page = BeautifulSoup(html, 'html.parser')
+page = BeautifulSoup(html, 'lxml')
 page.prettify()
 
 tables = page.find_all('table', width='515')    # looking for the one specific table that contains data about cities
 trRows = []
-#print(tables)
-
+trTags = []
+cities = {}
 
 for table in tables:
     trTags = table.find_all('tr')
@@ -16,4 +17,16 @@ for table in tables:
 # for i in trTags:
 #     trRows.append(i)
 
-print(trTags)
+for f in trTags:
+    trRows.append(f.find_all('font'))
+
+for i in trRows:
+    city = i[1].string
+    pop = i[3].string
+    cities[city] = pop
+
+# for i in trRows:
+#     print(i)
+# print(trRows[1][3].string)
+
+print(cities)
